124d6e5b18b8c7f5aa46b11afbbceca1b4680b5d
Ahmed Sameh <123486620+linguistc@users.noreply.github.com>
2024-09-01 00:28:58 +0300

    [1360] [Time Beats: 98.41%] [Memory Beats: 59.52%] - LeetPush

--- Manara/1360-Number-of-Days-Between-Two-Dates.cs
+++ Manara/1360-Number-of-Days-Between-Two-Dates.cs
@@ -1,10 +1,13 @@
 public class Solution {
     public int DaysBetweenDates(string date1, string date2) {
+        //Parsing
         var dt1 = DateTime.Parse(date1);
         var dt2 = DateTime.Parse(date2);
 
+        //TimeSpan object
         var timeSpan = dt1 - dt2;
 
+        //Day property
         var daysDiff = timeSpan.Days;
 
         return daysDiff < 0 ? -daysDiff : daysDiff;
