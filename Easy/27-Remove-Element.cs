c15d592c11db14d946d334d9b49747d71cfd09b9
Ahmed Sameh <123486620+linguistc@users.noreply.github.com>
2024-08-31 23:00:49 +0300

    [27] [Time Beats: 78.74%] [Memory Beats: 5.47%] - LeetPush

--- Easy/27-Remove-Element.cs
+++ Easy/27-Remove-Element.cs
@@ -1,15 +1,15 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
-        int[] expectedNums = new int[nums.Length];
-        short i =0, j = 0;
-        for (; i < nums.Length; ++i)
+        int k = 0;
+        for(int i = 0; i < nums.Length; i++)
         {
             if(nums[i] != val)
-                expectedNums[j++] = nums[i];
+            {
+                nums[k] = nums[i];
+                k++;
+            }
         }
-
-        Array.Copy(expectedNums, nums, j);
-
-        return j;
+        
+        return k;
     }
 }
