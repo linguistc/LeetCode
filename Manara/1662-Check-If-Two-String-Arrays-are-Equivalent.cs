cade1ed55f539cfd669c2787873e82d9b0bd7de1
Ahmed Sameh <123486620+linguistc@users.noreply.github.com>
2024-09-01 07:36:53 +0300

    [1662] [Time Beats: 96.36%] [Memory Beats: 73.35%] - LeetPush

--- Manara/1662-Check-If-Two-String-Arrays-are-Equivalent.cs
+++ Manara/1662-Check-If-Two-String-Arrays-are-Equivalent.cs
@@ -1,13 +1,42 @@
 public class Solution {
     public bool ArrayStringsAreEqual(string[] word1, string[] word2) {
-        // Concatenate all elements of word1
-        string s1 = string.Join("", word1);
+        // Initialize indices for word1 and word2 arrays
+        short w1 = 0 , w2 = 0;
+       // Initialize indices for characters within the current words
+        short i = 0, j = 0;
 
-        // Concatenate all elements of word2
-        string s2 = string.Join("", word2);
+        // Loop until we've exhausted either word1 or word2 arrays
+        while(w1 < word1.Length && w2 < word2.Length)
+        {
+            // If characters don't match, return false immediately
+            if(word1[w1][i] != word2[w2][j])
+                return false;
+            
+            // Move to the next character in both current words
+            ++i;
+            ++j;
 
-        // Compare the two concatenated strings
-        return s1 == s2;
+            // Move to the next word if you've reached the end of the current word 
+            if(i == word1[w1].Length)
+            {
+                ++w1;
+                i = 0;
+            }
+
+            if(j == word2[w2].Length)
+            {
+                ++w2;
+                j = 0;
+            }
+        }
+
+        // After exiting the loop, check if both arrays are fully traversed
+        // If either array hasn't been fully traversed, return false
+        if(w1 != word1.Length || w2 != word2.Length)
+            return false;
+
+        // If both arrays are fully traversed and matched, return true
+        return true;
 
     }
 }
